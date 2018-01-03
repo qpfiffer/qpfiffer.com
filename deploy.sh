ff2405ca0bbcb8d99a08f59ee67e0a9f5f4c28b4
Quinlan Pfiffer <qpfiffer@gmail.com>
2018-01-03 09:11:50 -0800

    Add static thing

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 rsync -Pavz _site/* qpfiffer.com:/var/www/qpfiffer.com/
 ./parse_lifts.py
 rsync -Pavz static/lifts.csv shithouse.tv:/var/www/qpfiffer.com/static/
+rsync -Pavz static shithouse.tv:/var/www/qpfiffer.com/static
 #rsync -Pavz static/robots.txt shithouse.tv:/var/www/qpfiffer.com/static/
 git commit -p
 git push
