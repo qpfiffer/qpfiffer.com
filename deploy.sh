0aa7e96d6c8798a1e3ee66788b808f044fe84b06
Quinlan Pfiffer <qpfiffer@gmail.com>
2018-02-11 20:49:21 -0800

    Make deploys faster because they're ASYNCHRONOUS

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 .cabal-sandbox/bin/qpfiffer.com build
-rsync -Pavz _site/* qpfiffer.com:/var/www/qpfiffer.com/
 ./parse_lifts.py
-rsync -Pavz static/lifts.csv shithouse.tv:/var/www/qpfiffer.com/static/
-rsync -Pavz static shithouse.tv:/var/www/qpfiffer.com/static
+rsync -Pavz _site/* qpfiffer.com:/var/www/qpfiffer.com/ &
+rsync -Pavz static/lifts.csv shithouse.tv:/var/www/qpfiffer.com/static/ &
+rsync -Pavz static shithouse.tv:/var/www/qpfiffer.com/static &
 #rsync -Pavz static/robots.txt shithouse.tv:/var/www/qpfiffer.com/static/
 git commit -p
 git push
