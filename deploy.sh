cd10dc488044e271cfbf3d41f6245fc91a92010b
Quinlan Pfiffer <qpfiffer@gmail.com>
2018-02-25 11:30:33 -0800

    Better script, dont need to commit every time

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,10 @@ rsync -Pavz _site/* qpfiffer.com:/var/www/qpfiffer.com/ &
 rsync -Pavz static/lifts.csv shithouse.tv:/var/www/qpfiffer.com/static/ &
 rsync -Pavz static shithouse.tv:/var/www/qpfiffer.com/static &
 #rsync -Pavz static/robots.txt shithouse.tv:/var/www/qpfiffer.com/static/
-git commit -p
-git push
-
+echo $1
+if [[ "$1" ]]; then
+    if [[ "$1" == "-p" ]]; then
+        git commit -p
+        git push
+    fi
+fi
